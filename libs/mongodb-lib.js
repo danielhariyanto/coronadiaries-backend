7c93a2afa0f200134cd01b6108141a7105715a50
dodellmitchell@gmail.com <dodellmitchell@gmail.com>
2020-09-12 10:51:52 -0700

    Removed and updated database credentials

--- libs/mongodb-lib.js
+++ libs/mongodb-lib.js
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose';
-const uri = "mongodb+srv://dbUser:admin@corona-diaries.9b0oy.mongodb.net/corona-diaries?retryWrites=true&w=majority";
 
 export default function(callback){
-    let db = mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
+    let db = mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
         .then(() => console.log("Connected!"))
         .catch(err => console.log(err));
 
